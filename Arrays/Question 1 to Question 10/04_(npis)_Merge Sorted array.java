e43a7bfa3ed15847897cffb6ae44670a645256ff
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-02-01 11:32:00 +0530

    Create 04_(npis)_Merge Sorted array.java

---
+++ Arrays/Question 1 to Question 10/04_(npis)_Merge Sorted array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=m+n-1,a=m-1,b=n-1;b>=0;i--){
+            if(a>=0 && nums1[a]>nums2[b]){
+                nums1[i]=nums1[a--];
+            }
+            else{
+                nums1[i]=nums2[b--];
+            }
+        }
+    }
+}
