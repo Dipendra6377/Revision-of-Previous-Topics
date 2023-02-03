8e8163d11c2cc301edd9504c24935fa873ce13ef
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-02-03 17:18:21 +0530

    Update 09_3 sum.java

--- Arrays/Question 1 to Question 10/09_3 sum.java
+++ Arrays/Question 1 to Question 10/09_3 sum.java
@@ -1,4 +1,4 @@
-// can be write in better way to improve run time
+// can be write in better way to improve run time below code
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
@@ -14,6 +14,7 @@ class Solution {
                 if(nums[i]+nums[j]+nums[k]==0){
                     set.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     k--;
+                    j++;
                 }
                 else if (nums[i]+nums[j]+nums[k]<0){
                     j++;
@@ -25,5 +26,42 @@ class Solution {
 
         }
         return new ArrayList<>(set);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res= new ArrayList<List<Integer>>();
+
+        Set<List<Integer>> set =new HashSet<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            int j=i+1;  int k=nums.length-1;
+
+            while(j<k){
+                if(nums[j]+nums[k]+nums[i]==0){
+                    set.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k && nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    j++;
+                    while(j<k && nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    k--;
+                }
+
+                else if(nums[j]+nums[k]+nums[i]>0){
+                    k--;
+                }
+                else{
+                    j++;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+
     }
 }
