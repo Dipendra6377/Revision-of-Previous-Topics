aa009066fa6165f81a6a5a0cac55503680244177
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-04-01 15:38:06 +0530

    Create 12_Rotate 2d Matrix.java

---
+++ Arrays/Question 1 to Question 10/12_Rotate 2d Matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.println(i);
+            for(int j=0;j<m/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
+            }
+        }
+    }
+}
